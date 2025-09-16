2d74b7717780abe0097157dbf7921ed4490a3d37
rishab <rrishab2001@gmail.com>
2025-09-16 17:51:37 +0200

    support for saving images and choice of hamiltonian/bloch

--- rabi_time_evol.py
+++ rabi_time_evol.py
@@ -6,6 +6,8 @@ from qutip.measurement import measure,measurement_statistics
 
 #hbar = 1.05457182e-34
 hbar = 1
+choose_hamiltonian = 1 #0 for RWA, 1 for non-RWA
+choose_bloch = 1 #0 for population plot, 1 for bloch sphere
 
 def HRWA(w0,w,omega0):
     return hbar * 0.5*((w-w0) * sigmaz()  + omega0 * sigmax())
@@ -18,38 +20,39 @@ def H_time_independent(w0,omega0):
     H1 = hbar * omega0 * sigmax()
     return [H0,H1]
 
-w0 = 20
-omega0 = 1
-detuning = [0]
+w0 = 20 #GHz
+omega0 = 1 #GHz
+detuning = [0,2]
 
-timesteps = np.linspace(0,2*np.pi/omega0,1000)
+timesteps = np.linspace(0,2*np.pi/omega0,1000) #nanosecond scale
 
 psi0 = basis(2,1)
 e_state = basis(2,1)
 results = []
-
 for delta in detuning:
 
-    #H = HRWA(w0,w0+delta,omega0)
-    H_temp = H_time_independent(w0,omega0)
-    H0 = H_temp[0]
-    H1 = H_temp[1]
-
-    args = {'w':w0+delta}    
-
-    H = QobjEvo([H0,[H1,H_time_function]],args = args,tlist = timesteps)
+    if(choose_hamiltonian):
+        H_temp = H_time_independent(w0,omega0)
+        H0 = H_temp[0]
+        H1 = H_temp[1]
+        args = {'w':w0+delta}    
+        H = QobjEvo([H0,[H1,H_time_function]],args = args,tlist = timesteps)
+    else:
+        H = HRWA(w0,w0+delta,omega0)
 
 
     res = sesolve(H, psi0, timesteps)
 
     results.append(res)
 
-epops = [[],[],[],[]]
+
+
+epops = [[],[]]
 
 i = 0
 
-b = [Bloch(),Bloch(),Bloch(),Bloch()]
-colors = ['r','g','b','y']
+b = [Bloch(),Bloch()]
+colors = ['r','g']
 
 while(i < len(detuning)):
     delta = detuning[i]
@@ -68,7 +71,18 @@ while(i < len(detuning)):
 
 
 i = 0
-while(i < len(detuning)):
-    b[i].show()
-    plt.show()
-    i+=1
+
+if(choose_bloch):
+    while(i < len(detuning)):
+        b[i].show()
+        plt.savefig(f'../op_figs/bloch_{choose_hamiltonian}_{detuning[i]}.png')
+        i += 1
+else:
+    while(i < len(detuning)):
+        plt.plot(timesteps,epops[i], label=r'$\Delta$ = ' + f'{detuning[i]}' + r'$\cdot \Omega_0$')
+        i+=1
+    plt.xlabel('Time (ns)')
+    plt.ylabel(r'Population of $|e\rangle$')
+    plt.legend(loc='lower right')
+    plt.title(r'$\omega_0$ = ' + f'{w0} GHz' + r', $\Omega_0$ = ' + f'{omega0} GHz')
+    plt.savefig(f'../op_figs/population_{choose_hamiltonian}.png')
